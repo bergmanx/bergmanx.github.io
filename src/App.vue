d440faf36a01b2ed408fcec05c13ce64cbd488cd
bergmanx <bergman.webdev@gmail.com>
2024-08-11 12:24:04 -0400

    Integration with ContactMe backend

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,55 @@
 <script setup>
+import { reactive } from "vue";
 const name = "Bergman";
 const socials = [{ icon: "mdi-github", link: "https://github.com/bergmanx" }];
 const stackImages = ["python.png", "vue.png", "javascript.png"];
 const projects = [];
 const parallax = "parallax.png";
+
+const form = reactive({
+    name: null,
+    email: null,
+    email: null,
+    _hidden: "",
+    loading: false
+});
+
+const snackbar = reactive({ active: false, message: null, color: null });
+
+function onSubmit() {
+    form.loading = true;
+    fetch(import.meta.env.VITE_CONTACTME_URL, {
+        method: "POST",
+        body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            message: form.message,
+            honeypot: form._hidden
+        }),
+        headers: { "Content-Type": "application/json" }
+    })
+        .then(response => {
+            if (response.status == 201) {
+                return response.json();
+            }
+            snackbar.message =
+                "An error has occurred. Check the form and try again";
+            snackbar.color = "red";
+            snackbar.active = true;
+            form.loading = false;
+            return response;
+        })
+        .then(response => {
+            if (response.status == "success") {
+                snackbar.message =
+                    "Thank you for contact me. I'll write you soon";
+                snackbar.color = "success";
+                snackbar.active = true;
+                form.loading = false;
+            }
+        });
+}
+
 oncontextmenu = () => false;
 </script>
 
@@ -44,27 +90,49 @@ oncontextmenu = () => false;
             </v-container>
             <v-container class="mb-4">
                 <v-theme-provider theme="light">
-                    <v-form>
+                    <v-form @submit.prevent="onSubmit" :disabled="form.loading">
                         <v-text-field
+                            v-model="form.name"
+                            name="name"
                             placeholder="Your name"
                             variant="solo"
                             rounded
                         ></v-text-field>
                         <v-text-field
+                            v-model="form.email"
+                            name="email"
                             type="email"
                             placeholder="Your email"
                             variant="solo"
                             rounded
                         ></v-text-field>
                         <v-textarea
+                            v-model="form.message"
+                            name="message"
                             placeholder="Message"
                             variant="solo"
                             rounded
                         ></v-textarea>
-                        <v-btn color="primary" block rounded>Contact me</v-btn>
+                        <v-text-field
+                            class="d-none"
+                            name="_hidden"
+                            v-model="form._hidden"
+                            type="hidden"
+                        ></v-text-field>
+                        <v-btn
+                            type="submit"
+                            color="primary"
+                            :loading="form.loading"
+                            block
+                            rounded
+                            >Contact me</v-btn
+                        >
                     </v-form>
                 </v-theme-provider>
             </v-container>
+            <v-snackbar :color="snackbar.color" v-model="snackbar.active">{{
+                snackbar.message
+            }}</v-snackbar>
         </v-parallax>
     </v-app>
 </template>
